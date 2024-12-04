43badea2d49405a9adb52e9b1f57a16a6842ff6e
mikhail <mikhailvitoshnev@hotmail.com>
2024-12-04 15:43:38 +0200

    fix workflow and create Makefiles

---
+++ app/src/test/java/hexlet/code/AppTest.java
@@ -0,0 +1,15 @@
+package hexlet.code;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class AppTest {
+
+    @Test
+    void main() {
+        String expected = "Hello, world";
+        String actual = "Hello, world";
+        assertEquals(expected, actual);
+    }
+}
