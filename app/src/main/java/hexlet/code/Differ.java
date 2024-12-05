49bb52041ad59621d28035763bfe0962133f4f69
MikVito <mikhailvitoshnev@hotmail.com>
2024-12-05 20:13:39 +0200

    init Differ class which control all process before call() method

--- app/src/main/java/hexlet/code/Differ.java
+++ app/src/main/java/hexlet/code/Differ.java
@@ -7,7 +7,14 @@ import java.nio.file.Paths;
 
 public class Differ {
     public static String generate(String filepath1, String filepath2, String format) throws Exception {
-        return "";
+        var file1 = fileReader(filepath1);
+        var file2 = fileReader(filepath2);
+
+        var getFileFormat1 = Parser.getJasonData(file1);
+        var getFileFormat2 = Parser.getJasonData(file2);
+
+        var compareFile = Tree.generateDifferenceTree(getFileFormat1, getFileFormat2);
+        return Format.formatDifference(compareFile);
     }
 
     public static String fileReader(String file) throws Exception {
--- app/src/main/java/hexlet/code/Parser.java
+++ app/src/main/java/hexlet/code/Parser.java
@@ -3,23 +3,22 @@ package hexlet.code;
 import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
-import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.util.HashMap;
 
-public class TypeReader {
-    public static HashMap<String, Object> getJasonFile(Path data) throws IOException {
-        String content = Files.readString(data);
+public class Parser {
+    public static HashMap<String, Object> getJasonData(String data) throws Exception {
+        String content = Files.readString(Path.of(data));
         ObjectMapper objectMapper = new ObjectMapper();
         return objectMapper.readValue(content, new TypeReference<>() {
         });
     }
 
-    public static HashMap<String, Object> getYmlFile(Path data) throws Exception {
+    public static HashMap<String, Object> getYamlData(Path data) throws Exception {
         String content = Files.readString(data);
-        ObjectMapper objectMapper = new ObjectMapper();
-        return objectMapper.readValue(content, new TypeReference<>() {
+        ObjectMapper yamlMapper  = new ObjectMapper();
+        return yamlMapper.readValue(content, new TypeReference<>() {
         });
     }
 }
