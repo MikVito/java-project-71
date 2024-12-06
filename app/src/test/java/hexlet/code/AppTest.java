6cc4a6f05af83244bdc7ede3e0605050550a0ce7
MikVito <mikhailvitoshnev@hotmail.com>
2024-12-07 00:21:04 +0200

    Rebuild test

--- app/src/test/java/hexlet/code/AppTest.java
+++ app/src/test/java/hexlet/code/AppTest.java
@@ -2,10 +2,14 @@ package hexlet.code;
 
 import org.junit.jupiter.api.Test;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 class AppTest {
 
     @Test
     void call() {
-        System.out.println(" ");
+        String expected = "expectedString";
+        String actual = "stylish";
+        assertEquals(expected, actual);
     }
 }
